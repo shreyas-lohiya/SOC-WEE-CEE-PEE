95f8326b43fd9d776d2dd24ff71d29beca1c3934
Shreyas Lohiya <lohiya.shreyas@gmail.com>
2026-06-25 21:17:51 +0530

    Create sum_of_two_values.cpp

---
+++ Problems Practiced/cses/sum_of_two_values.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+using vi = vector<int>;
+using pii = pair<int,int>;
+using vpi = vector<pii>;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define sortv(x) sort(all(x))
+#define sortr(x) sort(rall(x))
+#define sortp2(x) sort(all(x),[](const auto& a, const auto& b){if(a.second==b.second) return a.first<b.first; return a.second<b.second;})
+#define sortp2r(x) sort(all(x),[](const auto& a, const auto& b){if(a.second==b.second) return a.first>b.first; return a.second>b.second;})
+template<class T1, class T2>
+istream& operator>>(istream& in, pair<T1,T2>& p){
+    in >> p.first >> p.second;
+    return in;
+}
+ 
+template<class T1, class T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p){
+    return out << "\n" << p.first << " " << p.second;
+}
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto &x:v) in>>x;
+    return in;
+}
+template<class T>
+ostream& operator<<(ostream& out, const vector<T>& v){
+    for(auto &x:v) out<<x<<" ";
+    return out;
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,x; cin>>n>>x; vpi a(n); for(int i=0; i<n; i++) cin>>a[i].first,a[i].second=i+1; sortv(a);
+    int l=0,r=n-1;
+    while(r>l){
+        if(a[l].first+a[r].first<x) l++;
+        else if(a[l].first+a[r].first>x) r--;
+        else {cout<<a[l].second<<" "<<a[r].second<<"\n"; break;}
+    }
+    if(r==l) cout<<"IMPOSSIBLE"<<"\n";
+}
