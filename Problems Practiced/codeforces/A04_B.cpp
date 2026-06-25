2958ca34b79aac03d30133a317df4c5ee10bef25
Shreyas Lohiya <lohiya.shreyas@gmail.com>
2026-06-25 21:59:37 +0530

    Create A04_B.cpp

---
+++ Problems Practiced/codeforces/A04_B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+using vi = vector<int>;
+using pii = pair<int,int>;
+using vpi = vector<pii>;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define sortv(x) sort(all(x))
+#define sortr(x) sort(rall(x))
+template<class T1, class T2>
+istream& operator>>(istream& in, pair<T1,T2>& p){
+    in >> p.first >> p.second;
+    return in;
+}
+
+template<class T1, class T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p){
+    return out << "\n" << p.first << " " << p.second;
+}
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto &x:v) in>>x;
+    return in;
+}
+template<class T>
+ostream& operator<<(ostream& out, const vector<T>& v){
+    for(auto &x:v) out<<x<<" ";
+    return out;
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t=1; cin>>t;
+    while(t--){
+        int n; cin>>n; vi a(n); cin>>a; int lc=1; int i=0;
+        for(; i<n; i++){
+            lc=lcm(lc,i+2);
+            if(a[i]%lc==0) break;
+        }
+        cout<<(i==n?"YES":"NO")<<"\n";
+    }
+}
