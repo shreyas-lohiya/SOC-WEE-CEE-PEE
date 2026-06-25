4b10cc1f717d1cc18b2145e0119192bd95aea92a
Shreyas Lohiya <lohiya.shreyas@gmail.com>
2026-06-25 21:19:45 +0530

    Create collecting_numbers.cpp

---
+++ Problems Practiced/cses/collecting_numbers.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+using vi = vector<int>;
+using pii = pair<int,int>;
+using vpi = vector<pii>;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define sortv(x) sort(all(x))
+#define sortr(x) sort(rall(x))
+#define sortp2(x) sort(all(x),[](const auto& a, const auto& b){if(a.second==b.second) return a.first<b.first; return a.second<b.second;})
+#define sortp2r(x) sort(all(x),[](const auto& a, const auto& b){if(a.second==b.second) return a.first>b.first; return a.second>b.second;})
+template<class T1, class T2>
+istream& operator>>(istream& in, pair<T1,T2>& p){
+    in >> p.first >> p.second;
+    return in;
+}
+ 
+template<class T1, class T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p){
+    return out << "\n" << p.first << " " << p.second;
+}
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto &x:v) in>>x;
+    return in;
+}
+template<class T>
+ostream& operator<<(ostream& out, const vector<T>& v){
+    for(auto &x:v) out<<x<<" ";
+    return out;
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,temp,ans=0; cin>>n; vector<bool> x(n+1,1);
+    while(n--){
+        cin>>temp;
+        ans+=x[temp-1];
+        x[temp]=0;
+    }
+    cout<<ans<<"\n";
+}
