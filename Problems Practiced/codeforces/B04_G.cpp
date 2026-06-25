92081946c26b5c631a97f82c083425c4dafcb548
Shreyas Lohiya <lohiya.shreyas@gmail.com>
2026-06-25 21:57:36 +0530

    Create B04_G.cpp

---
+++ Problems Practiced/codeforces/B04_G.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+using vi = vector<int>;
+using pii = pair<int,int>;
+using vpi = vector<pii>;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define sortv(x) sort(all(x))
+#define sortr(x) sort(rall(x))
+template<class T1, class T2>
+istream& operator>>(istream& in, pair<T1,T2>& p){
+    in >> p.first >> p.second;
+    return in;
+}
+
+template<class T1, class T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p){
+    return out << "\n" << p.first << " " << p.second;
+}
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto &x:v) in>>x;
+    return in;
+}
+template<class T>
+ostream& operator<<(ostream& out, const vector<T>& v){
+    for(auto &x:v) out<<x<<" ";
+    return out;
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t=1; cin>>t;
+    while(t--){
+        int n,x; cin>>n>>x; vi a(n); cin>>a;
+        int ans=1; set<int> s; s.insert(x);
+        for(int i=0; i<n; i++){
+            if(x%a[i]==0){
+                if(s.contains(a[i])){
+                    ans++;
+                    s.clear();
+                    s.insert(x);
+                    s.insert(x/a[i]);
+                }
+                else{
+                    vi temp;
+                    for(int val: s){
+                        if(val%a[i]==0){
+                            temp.push_back(val/a[i]);
+                        }
+                    }
+                    s.insert(all(temp));
+                }
+            }
+        }
+        cout<<ans<<"\n";
+    }
+}
