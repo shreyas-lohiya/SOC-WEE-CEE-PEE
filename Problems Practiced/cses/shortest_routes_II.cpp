22fda5e33a07ee144682318f9c8072afb7e24419
Shreyas Lohiya <lohiya.shreyas@gmail.com>
2026-07-23 23:01:34 +0530

    Create shortest_routes_II.cpp

---
+++ Problems Practiced/cses/shortest_routes_II.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+using vi = vector<int>;
+using pii = pair<int,int>;
+using vpi = vector<pii>;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+#define sortv(x) sort(all(x))
+#define sortr(x) sort(rall(x))
+#define sortp2(x) sort(all(x),[](const auto& a, const auto& b){if(a.second==b.second) return a.first<b.first; return a.second<b.second;})
+#define sortp2r(x) sort(all(x),[](const auto& a, const auto& b){if(a.second==b.second) return a.first>b.first; return a.second>b.second;})
+template<class T1, class T2>
+istream& operator>>(istream& in, pair<T1,T2>& p){
+    in >> p.first >> p.second;
+    return in;
+}
+template<class T1, class T2>
+ostream& operator<<(ostream& out, const pair<T1,T2>& p){
+    return out << "\n" << p.first << " " << p.second;
+}
+template<class T>
+istream& operator>>(istream& in, vector<T>& v){
+    for(auto &x:v) in>>x;
+    return in;
+}
+template<class T>
+ostream& operator<<(ostream& out, const vector<T>& v){
+    for(auto &x:v) out<<x<<" ";
+    return out;
+}
+int ipow(int base, unsigned int exp) {
+    int result = 1; int mod=998244353;
+    while (exp > 0){
+        if (exp & 1) {
+            result*=base;
+            result%=mod;
+        }
+        base*=base;
+        base%=mod;
+        exp/=2;
+    }
+    return result;
+}
+int find(int x, const vi& link) {
+    while (x != link[x]) x = link[x];
+    return x;
+}
+bool same(int a, int b, const vi& link) {
+    return find(a,link) == find(b,link);
+}
+void unite(int a, int b, vi& link, vi& size) {
+    a = find(a,link);
+    b = find(b,link);
+    if (size[a] < size[b]) swap(a,b);
+    size[a] += size[b];
+    link[b] = a;
+}
+//segtreeeee
+int IDENTITY=0;                     //changethis!!!!!!!
+int segfunc(int a, int b){
+    return a+b;                     //changethis!!!!!!!
+}
+int N; vi segtree;
+void makesegtree(const int& n, const vi& a){
+    N=bit_ceil((unsigned)n);
+    segtree.assign(2*N, IDENTITY);
+    for(int i=0;i<n;i++) 
+        segtree[N+i]=a[i];
+    for(int i=N-1;i>=1;i--)
+        segtree[i]=segfunc(segtree[2*i],segtree[2*i+1]);
+}
+int query(int l, int r){
+    l+=N; r+=N;
+    int s=IDENTITY;
+    while(l<=r){
+        if(l%2==1) s=segfunc(s,segtree[l++]);
+        if(r%2==0) s=segfunc(s,segtree[r--]);
+        l/=2; r/=2;
+    }
+    return s;
+}
+void update(int k, int x){
+    k+=N;
+    segtree[k]=x;
+    for(k/=2; k>=1; k/=2){
+        segtree[k]=segfunc(segtree[2*k],segtree[2*k+1]);
+    }
+}
+//segtree finish
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m,q,a,b,w; cin>>n>>m>>q; vector<vi> adj(n+1,vi(n+1,1e18));
+    while(m--){
+        cin>>a>>b>>w;
+        adj[a][b]=min(adj[a][b],w);
+        adj[b][a]=min(adj[a][b],w);
+    }
+    vector<vi> distance(n+1,vi(n+1));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i == j) distance[i][j] = 0;
+            else if (adj[i][j]) distance[i][j] = adj[i][j];
+            else distance[i][j] = 1e18;
+        }
+    }
+    for (int k = 1; k <= n; k++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                distance[i][j] = min(distance[i][j],distance[i][k]+distance[k][j]);
+            }
+        }
+    }
+    while(q--){
+        cin>>a>>b; cout<<(distance[a][b]==1e18?-1:distance[a][b])<<"\n";
+    }
+}
